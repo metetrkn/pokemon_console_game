6c0a459458da11704b74091e427a97d016fc09f6
= <=>
2024-05-13 00:21:58 +0200

    team pokemon chooses some bugs fixed

--- play.py
+++ play.py
@@ -39,31 +39,31 @@ class Pokemon:
 
 class Fight:
     def start_fight(self, warrior1, worrior2):         
-        attacker = warrior1 if self.current_turn == 0 else worrior2
-        defender = worrior2 if self.current_turn == 0 else warrior1
+        self.attacker = warrior1 if self.current_turn == 0 else worrior2
+        self.defender = worrior2 if self.current_turn == 0 else warrior1
 
         while True:
-            print(f"It's {attacker.name}'s turn.")
+            print(f"It's {self.attacker.name}'s turn.")
             choice = input("Enter 1 to attack, 2 to revive, 3 to quit: ")
             if choice == '1':
-                damage, random_factor, move_type = attacker.calculate_damage(defender)
+                damage, random_factor, move_type = self.attacker.calculate_damage(self.defender)
                 if random_factor == 0:
                     print("Attack Missed!")
                 elif random_factor > 1:
                     print("Critical Hit!")
-                defender.current_health = round(max(0, defender.current_health - damage), 1)
-                print(f"{attacker.name} attacks with {damage} PP, {defender.name} got injured.")
+                self.defender.current_health = round(max(0, self.defender.current_health - damage), 1)
+                print(f"{self.attacker.name} attacks with {damage} PP, {self.defender.name} got injured.")
                 print(f"{warrior1.name} health = {warrior1.current_health} HP, {worrior2.name} health = {worrior2.current_health} HP")
                 print("="*50)
 
-                if defender.current_health <= 0:
-                    print(f"{defender.name} fainted!")
+                if self.defender.current_health <= 0:
+                    print(f"{self.defender.name} fainted!")
                     
                     # Updating teams with reamining pokemonss
                     self.teams_remainings()
 
             elif choice == '2':
-                attacker.revive()
+                self.attacker.revive()
                 print(f"{warrior1.name} health = {round(warrior1.current_health, 1)} HP, {worrior2.name} health = {round(worrior2.current_health, 1)} HP")
                 print("="*50)
             elif choice == '3':
@@ -74,8 +74,8 @@ class Fight:
                 print("="*50)
 
             self.switch_turn()
-            attacker = warrior1 if self.current_turn == 0 else worrior2
-            defender = worrior2 if self.current_turn == 0 else warrior1
+            self.attacker = warrior1 if self.current_turn == 0 else worrior2
+            self.defender = worrior2 if self.current_turn == 0 else warrior1
 
 
     def __init__(self, pokTeam1, pokTeam2):
@@ -112,58 +112,72 @@ class Fight:
         self.current_turn = (self.current_turn + 1) % 2
 
     def teams_remainings(self):
-        # New teams list which contains that pokemons have more than 0 HP
-        remainings_t1 = []
-        remainings_t2 = []
-
-        # Adding new teams list with not fainted pokemons
-        # Number of remaining pokemon per team
-        num1 = len(self.pokTeam1)
-        num2 = len(self.pokTeam2)
-
-        # Updating Team1
-        for i in range(num1):
-            if self.pokTeam1[i].current_health > 0:
-                remainings_t1.append(self.pokTeam1[i])
-        
-        # Updating Team2
-        for i in range(num2):
-            if self.pokTeam2[i].current_health > 0:
-                remainings_t2.append(self.pokTeam2[i])     
-
-        # If one of teams all pokemon is fainted, other player wins!
-        if len(remainings_t1) == 0:
-            print("Team2 has winned the game! Congrats!!!")
-            exit()
-        elif len(remainings_t2) == 0:
-            print("Team1 has winned the game! Congrats!!!")
-            exit()
-
-        # Updating teams with not fainted pokemons
-        self.pokTeam1 = remainings_t1 
-        self.pokTeam2 = remainings_t2 
+        if self.current_turn == 0:
+            # Team2's new list of pokemon which contains that pokemons have more than 0 HP
+            remainings_t2 = []
+            # Number of pokemon for team2 old version
+            num2 = len(self.pokTeam2)
+            # Updating Team2
+            for i in range(num2):
+                if self.pokTeam2[i].current_health > 0:
+                    remainings_t2.append(self.pokTeam2[i])  
+            # If there is no unfainted pokemons in the team, other team wins and game finishes
+            if len(remainings_t2) == 0:
+                print("Team1 has winned the game! Congrats!!!")
+                exit()
+            # Updating teams with not fainted pokemons
+            self.pokTeam2 = remainings_t2 
+                    
+        else:
+            # Team1's new list of pokemon which contains that pokemons have more than 0 HP
+            remainings_t1 = []
+            # Number of pokemon for team1 old version
+            num1 = len(self.pokTeam1)
+            # Updating Team1
+            for i in range(num1):
+                if self.pokTeam1[i].current_health > 0:
+                    remainings_t1.append(self.pokTeam1[i])
+            # If there is no unfainted pokemons in the team, other team wins and game finishes
+            if len(remainings_t1) == 0:
+                print("Team2 has winned the game! Congrats!!!")
+                exit()
+            # Updating teams with not fainted pokemons
+            self.pokTeam1 = remainings_t1 
 
         # Fainted pokemon's team chooses new pokemon for next round
         self.choosing_to_fight()
 
     def choosing_to_fight(self):  
-        # Team1 chooses its new pokemon for new round
-        which_pokemon = 0
-        for pokemon_remaining in self.pokTeam1:
-            print(which_pokemon, ": ", pokemon_remaining.name, "\t\t", end="")
-            which_pokemon += 1
-        warrior1 = int(input("Choose\t:"))
-        print(f"{self.pokTeam1[warrior1].name} I choose you!")
-        warrior1 = self.pokTeam1[warrior1]
+        # If fainted defender was in team1
+        if self.current_turn == 1:
+            print("-"*50)
+            print("Team1 chooses new pokemon")
+            print("-"*50)
+            # Team1 chooses its new pokemon for new round
+            which_pokemon = 0
+            for pokemon_remaining in self.pokTeam1:
+                print(which_pokemon, ": ", pokemon_remaining.name, "\t\t", end="")
+                which_pokemon += 1
+            warrior1 = int(input("Choose\t:"))
+            print(f"{self.pokTeam1[warrior1].name} I choose you!")
+            warrior1 = self.pokTeam1[warrior1] # choson pokemon instead of fainted defender
+            warrior2 = self.attacker # current attacker
+            
 
-        # Team2 chooses its new pokemon for new round
-        which_pokemon = 0
-        for pokemon_remaining in self.pokTeam2:
-            print(which_pokemon, ": ", pokemon_remaining.name, "\t\t", end="")
-            which_pokemon += 1
-        warrior2 = int(input("Choose\t:"))
-        print(f"{self.pokTeam2[warrior2].name} I choose you!")
-        warrior2 = self.pokTeam2[warrior2]
+        else:
+            # If fainted defender was in team2
+            print("-"*50)
+            print("Team2 chooses new pokemon")
+            print("-"*50)
+            # Team2 chooses its new pokemon for new round
+            which_pokemon = 0
+            for pokemon_remaining in self.pokTeam2:
+                print(which_pokemon, ": ", pokemon_remaining.name, "\t\t", end="")
+                which_pokemon += 1
+            warrior2 = int(input("Choose\t:"))
+            print(f"{self.pokTeam2[warrior2].name} I choose you!")
+            warrior2 = self.pokTeam2[warrior2] # chosen pokemon instead of fainted defender
+            warrior1 = self.attacker # current attacker
 
         # New round begins with newly choosen pokemons
         self.start_fight(warrior1, warrior2)
